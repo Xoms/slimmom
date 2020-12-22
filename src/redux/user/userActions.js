d2674c25c12788e5bfb551574f1b92955a5e6fe1
Ostap Pluzhnik <plostvik@gmail.com>
2020-12-22 14:56:17 +0200

    fix

--- src/redux/user/userActions.js
+++ src/redux/user/userActions.js
@@ -3,10 +3,17 @@ import { createAction } from '@reduxjs/toolkit';
 const getCurrentUserRequest = createAction('auth/getUserRequest');
 const getCurrentUserSuccess = createAction('auth/getUserSuccess');
 const getCurrentUserError = createAction('auth/getUserError');
+const getDailyRateRequest = createAction('auth/getDailyRateRequest');
+const getDailyRateSuccess = createAction('auth/getDailyRateSuccess');
+const getDailyRateError = createAction('auth/getDailyRateError');
 
-const actions = { 
-    getCurrentUserRequest,
-    getCurrentUserSuccess,
-    getCurrentUserError
-}
-export default actions
+const actions = {
+  getCurrentUserRequest,
+  getCurrentUserSuccess,
+  getCurrentUserError,
+  getDailyRateRequest,
+  getDailyRateSuccess,
+  getDailyRateError,
+};
+
+export default actions;
