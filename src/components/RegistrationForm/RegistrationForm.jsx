6b083d4fbc13e81aafbf0867dd17a332007bdc95
ArtemSlipchencko <68220186+ArtemSlipchencko@users.noreply.github.com>
2020-12-22 21:55:59 +0200

    validation

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -3,47 +3,94 @@ import Button from '../shared/Button/Button';
 import ops from '../../redux/auth/authOperations';
 import {connect} from 'react-redux';
 import './RegistrationForm.scss';
+import Notification from "../shared/Notification/Notification";
+import {Formik, Form, Field, ErrorMessage} from "formik";
+import * as Yup from "yup";
+
+const RegisterSchema = Yup.object().shape({
+    username: Yup.string().min(2, "Too short!").max(50, "Too long!").required("Required!"),
+    email: Yup.string().min(2, "Too short!").max(50, "Too long!").required("Required!"),
+    password: Yup.string().required("Required!").min(8, "Too short!"),
+});
 
 class RegistrationForm extends Component {
-    state = {
-        email: '',
-        password: '',
-        username: '',
-    };
-
-    handleChange = ({target: {name, value}}) => {
-        this.setState({[name]: value})
-    };
-
+    
     handleClick = () => {
         this.props.history.push('/login')
     };
 
-    handleSubmit = (event) => {
-        event.preventDefault();
-        this.props.register(({...this.state}));
-        this.setState({
-            email: '',
-            password: '',
-            username: '',
-        });
-        this.props.history.push('/login');
+    handleSubmit = (values) => {
+        this.props.register(values)
     };
 
     render () {
         return (
-            <form onSubmit={this.handleSubmit} className="registration-form">
-                <h1>РЕГИСТРАЦИЯ</h1>
-                <div className="registration-inputs">
-                    <input onChange={this.handleChange} value={this.state.username} placeholder="Имя *" name="username" type="text"/>
-                    <input onChange={this.handleChange} value={this.state.email} placeholder="E-mail *" name="email" type="text"/>
-                    <input onChange={this.handleChange} value={this.state.password} placeholder="Пароль *" name="password" type="password"/>
-                </div>
-                <div className="registration-buttons">
-                    <Button clickHandler={this.handleClick} marker="secondary" text="Вход"/>
-                    <Button marker="primary" text="Регистрация"/>
-                </div>
-            </form>
+
+            <>
+                <Formik initialValues={{email: "", password: "", username: ""}} onSubmit={this.handleSubmit} validationSchema={RegisterSchema}>
+                <Form className="registration-form">
+                    <h1>РЕГИСТРАЦИЯ</h1>
+                    <div className="registration-inputs">
+                        <label className="formLabel">
+                            <Field className="input" type="text" name="username" placeholder="Имя *"/>
+                            <ErrorMessage className='validField' name="username" component="span" />
+                        </label>
+                        <label className="formLabel">
+                            <Field className="input" type="email" name="email" placeholder="E-mail *"/>
+                            <ErrorMessage className='validField' name="email" component="span" />
+                        </label>
+                        <label className="formLabel">
+                            <Field className="input" type="password" name="password" placeholder="Пароль *"/>
+                            <ErrorMessage className='validField' name="password" component="span" />
+                        </label>
+                    </div>
+                    <div className="registration-buttons">
+                        <Button clickHandler={this.handleClick} marker="secondary" text="Вход"/>
+                        <Button marker="primary" text="Регистрация"/>
+                    </div>
+                </Form>
+                </Formik>
+            </>
+
+    //   <Notification error={Boolean(props.error)} message="Заполните форму в соответствии с требованиями"></Notification>
+    //   <div className={css.loginPage}>
+    //     <h2 className={css.loginTitle}>Вход</h2>
+
+    //     <Formik initialValues={{email: "", password: ""}} onSubmit={handleSubmit} validationSchema={SignupSchema}>
+    //       <Form className={css.loginForm}>
+
+    //       <label className={css.formLabel}>
+    //           <Field
+    //             className={css.login}
+    //             type="email"
+    //             name="email"
+    //             placeholder="Логин *"
+    //           />
+    //           <ErrorMessage className={css.validField} name="email" component="span" />
+    //         </label>
+
+    //         <label className={css.formLabel}>
+    //           <Field
+    //             className={css.password}
+    //             type="password"
+    //             name="password"
+    //             placeholder="Пароль *"
+    //           />
+    //           <ErrorMessage className={css.validField} name="password" component="span" />
+    //         </label>
+
+    //         <div className={css.buttons}>
+    //         <Button marker="primary" text="Вход"> </ Button>
+    //         <Button marker="secondary" text="Регистрация" >
+              
+    //           </ Button>
+            
+    //         </div>
+    //       </Form>
+    //     </Formik>
+    //   </div>
+
+           
         )
     }
 
