36c49e9c3ae13d3e7aedba76b907d1d3f8d83366
ArtemSlipchencko <68220186+ArtemSlipchencko@users.noreply.github.com>
2020-12-22 15:57:32 +0200

    logout

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,18 +1,32 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import ops from '../../redux/auth/authOperations';
 import './UserInfo.scss';
 
 class UserInfo extends Component {
+
+    handleClick = () => {
+        this.props.logout();
+    };
 
     render () {
         return (
             <div className="user-info-wrapper">
                 <p className="user-info-wrapper-item">{this.props.userName}</p>
                 <span className="user-info-wrapper-item"></span>
-                <button className="user-info-wrapper-item" type="button">Выйти</button>
+                <button onClick={this.handleClick} className="user-info-wrapper-item" type="button">Выйти</button>
             </div>
         )
-    }
+    };
 
 };
 
-export default UserInfo
+const mapStateToProps = state => ({
+    userName: state.user.username,
+});
+
+const mapDispatchToProps = {
+    logout: ops.logout,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserInfo);
