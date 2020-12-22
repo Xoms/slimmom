9a519ff96527576cca14f916f281ddb5ca21f027
IlyaLitvin <darnon88@gmail.com>
2020-12-22 14:09:37 +0200

    1th step

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,12 +1,8 @@
 //Заглушка для роутинга
-import React from 'react';
-
-
+import React from "react";
 
 const NotFound = (props) => (
-  <div className="NotFoundWrapper">
-    In development
-  </div>
+  <div className="NotFoundWrapper">In development</div>
 );
 
 export default NotFound;
--- src/services/backend.service.js
+++ src/services/backend.service.js
@@ -1,54 +1,61 @@
-import axios from 'axios';
+import axios from "axios";
 
-const url = "https://slimmom-backend.herokuapp.com/"; 
+const url = "https://slimmom-backend.herokuapp.com/";
 
 axios.defaults.baseURL = url;
 
 class PhonebookService {
+  //===================  auth ===================
 
-    //===================  auth ===================
+  register(newUser) {
+    return axios.post("/auth/register", newUser);
+  }
 
-    register(newUser){
-        return axios.post('/auth/register', newUser)
-    }
+  login(userCredentials) {
+    return axios.post("/auth/login", userCredentials);
+  }
 
-    login(userCredentials){
-        return axios.post('/auth/login', userCredentials)
-    }
+  logout() {
+    return axios.post("/auth/logout");
+  }
 
-    logout(){
-        return axios.post('/auth/logout')
-    }    
+  getCurrentUser() {
+    return axios.get("/user");
+  }
 
-    getCurrentUser(){
-        return axios.get('/user')
-    }
+  setToken(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  }
 
-    setToken(token){
-        axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-    }
+  unsetToken() {
+    axios.defaults.headers.common.Authorization = ``;
+  }
 
-    unsetToken(){
-        axios.defaults.headers.common.Authorization = ``;
-    }
+  getDailyRate(userCharacteristics) {
+    axios.defaults.headers.common.Authorization =
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI1ZmUxZGNiODVjMmJhNzAwMDQ0NDA5NjUiLCJzaWQiOiI1ZmUxZGNjMjVjMmJhNzAwMDQ0NDA5NjYiLCJpYXQiOjE2MDg2Mzc2MzQsImV4cCI6MTYwODY0MTIzNH0.LUdm7gIfo8gLD8EZWyRP5j8l225ZrWvpvqD0-Nb_lsg";
+    return axios.post(
+      "/daily-rate/5fcffaa7f7ae5300043515a6",
+      userCharacteristics
+    );
+  }
 
-    //================== contacts ==================
-    getContacts(){
-        return axios.get('/contacts'); 
-    }
+  //================== contacts ==================
+  getContacts() {
+    return axios.get("/contacts");
+  }
 
-    addContact(newContact) {
-        return axios.post('/contacts', newContact);
-    }
+  addContact(newContact) {
+    return axios.post("/contacts", newContact);
+  }
 
-    delContact(id) {
-        return axios.delete(`/contacts/${id}`)
-    }
-    
-    updateContact(id) {
-        return axios.patch(`/contacts/${id}`)
-    }
+  delContact(id) {
+    return axios.delete(`/contacts/${id}`);
+  }
 
+  updateContact(id) {
+    return axios.patch(`/contacts/${id}`);
+  }
 }
 
 export default new PhonebookService();
