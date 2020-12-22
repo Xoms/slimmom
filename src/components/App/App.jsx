bd29374266fbcf08a2b037bc8b7c318c2c0625a3
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2020-12-22 06:09:22 +0200

    wrapper_main_page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,29 +1,40 @@
-import React, {Component, Suspense, lazy} from 'react';
-import { Route, Switch, /*Redirect*/ } from 'react-router-dom';
+import React, { Component, Suspense, lazy, Fragment } from "react";
+import { Route, Switch /*Redirect*/ } from "react-router-dom";
 
-import routes from '../../routes';
-import PublicRoute from '../PublicRoute/PublicRoute';
-import PrivateRoute from '../PrivateRoute/PrivateRoute';
+import routes from "../../routes";
+import PublicRoute from "../PublicRoute/PublicRoute";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
 
-import Loader from '../shared/Loader';
+//jsx components
+import Loader from "../shared/Loader";
+import Decoration from "../Decoration";
 
-import './App.scss';
+//style
+import "./App.scss";
 
-class App extends Component{
+class App extends Component {
   render() {
-    const routesMap = routes.map(route => {
-      return route.privated ? <PrivateRoute key={route.path} {...route}/> : <PublicRoute key={route.path} {...route} />
-      })
+    const routesMap = routes.map((route) => {
+      return route.privated ? (
+        <PrivateRoute key={route.path} {...route} />
+      ) : (
+        <PublicRoute key={route.path} {...route} />
+      );
+    });
 
-  return (
-    <Suspense fallback={<Loader/>}>
-      <Switch>
-        {routesMap}
-        <Route component={lazy( () => import("../../pages/NotFound") )} />
-      </Switch>
-  </Suspense>
-  )}
-};
+    return (
+      <Fragment>
+        <Decoration isLoginPage={true}/>
+        <Suspense fallback={<Loader />}>
+          <Switch>
+            {routesMap}
+            <Route component={lazy(() => import("../../pages/NotFound"))} />
+          </Switch>
+        </Suspense>
+      </Fragment>
+    );
+  }
+}
 
 App.propTypes = {
   // bla: PropTypes.string,
