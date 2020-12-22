d2388700c25141346bb2cfbe678987eead62a64e
Ostap Pluzhnik <plostvik@gmail.com>
2020-12-22 12:12:13 +0200

    заканчивает компонент Layout, RegisterPage, LoginPage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,29 +1,37 @@
-import React, {Component, Suspense, lazy} from 'react';
-import { Route, Switch, /*Redirect*/ } from 'react-router-dom';
+import React, { Component, Suspense, lazy } from 'react';
+import { Route, Switch /*Redirect*/ } from 'react-router-dom';
 
 import routes from '../../routes';
 import PublicRoute from '../PublicRoute/PublicRoute';
 import PrivateRoute from '../PrivateRoute/PrivateRoute';
 
 import Loader from '../shared/Loader';
+import Layout from '../Layout';
 
 import './App.scss';
 
-class App extends Component{
+class App extends Component {
   render() {
     const routesMap = routes.map(route => {
-      return route.privated ? <PrivateRoute key={route.path} {...route}/> : <PublicRoute key={route.path} {...route} />
-      })
+      return route.privated ? (
+        <PrivateRoute key={route.path} {...route} />
+      ) : (
+        <PublicRoute key={route.path} {...route} />
+      );
+    });
 
-  return (
-    <Suspense fallback={<Loader/>}>
-      <Switch>
-        {routesMap}
-        <Route component={lazy( () => import("../../pages/NotFound") )} />
-      </Switch>
-  </Suspense>
-  )}
-};
+    return (
+      <Layout>
+        <Suspense fallback={<Loader />}>
+          <Switch>
+            {routesMap}
+            <Route component={lazy(() => import('../../pages/NotFound'))} />
+          </Switch>
+        </Suspense>
+      </Layout>
+    );
+  }
+}
 
 App.propTypes = {
   // bla: PropTypes.string,
---
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import RegistrationForm from '../../components/LoginForm';
+
+const RegistrationPage = () => (
+  <RegistrationForm>Test content</RegistrationForm>
+);
+
+export default RegistrationPage;
