38bfd9dd8dfca9e4e43bf069f68d0e79370232d3
VolodymyrKoch <volodymyrkocherhan@gmail.com>
2020-12-22 22:23:36 +0200

    fixed

--- src/components/RightSideBar/RightSideBar.jsx
+++ src/components/RightSideBar/RightSideBar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import classes from './rightSideBar.module.scss';
-// import imgLeaves from '../../img/rightSideBarBottom@1x.png'
 
 function RightSideBar({ props }) {
   return (
