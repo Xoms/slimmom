8610f1a48f385aca5915c82e76bec05db522bc2b
ludmiladev <45207186+ludmiladev@users.noreply.github.com>
2020-12-23 12:38:47 +0200

    fix

--- src/components/DiaryProductListItem/DiaryProductListItem.jsx
+++ src/components/DiaryProductListItem/DiaryProductListItem.jsx
@@ -2,6 +2,7 @@ import { connect } from "formik";
 import React from "react";
 import styles from "./DiaryProductListItem.module.scss";
 import {deleteEatenProduct} from "../../redux/user/userOperations";
+import close from "./image/close.svg";
 
 
 const DiaryProductListItem = ({ title, ccal, weight, dayId, productId, deleteProduct }) => {
@@ -14,7 +15,11 @@ return (
   <span className={styles.listCalories}>{ccal} ккал</span>
 
   <button className={styles.listButton} onClick={() => deleteProduct({dayId, productId})}>
-x
+  src={close}
+          alt="close menu"
+          aria-label="close menu"
+          className={styles.img}
+        />
       </button>
     </li>
   );
