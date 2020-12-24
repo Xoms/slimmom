fc021ab86b45dc8657ece7902cfe42e5bd37b974
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2020-12-24 22:46:44 +0200

    fix_markup_state3

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,102 +1,133 @@
-import React, {Component} from 'react';
-import Button from '../shared/Button/Button';
-import ops from '../../redux/auth/authOperations';
-import {connect} from 'react-redux';
-import css from './RegistrationForm.module.scss';
+import React, { Component } from "react";
+import Button from "../shared/Button/Button";
+import ops from "../../redux/auth/authOperations";
+import { connect } from "react-redux";
+import css from "./RegistrationForm.module.scss";
 import Notification from "../shared/Notification/Notification";
-import {Formik, Form, Field, ErrorMessage} from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
-import {authActions} from "../../redux/auth"
-import globalSelectors from "../../redux/global/globalSelectors"
+import { authActions } from "../../redux/auth";
+import globalSelectors from "../../redux/global/globalSelectors";
+import Decoration from "../Decoration";
 
 const RegisterSchema = Yup.object().shape({
-    username: Yup.string().min(2, "Too short!").max(50, "Too long!").required("Обязательное поле *"),
-    email: Yup.string().min(2, "Too short!").max(50, "Too long!").required("Обязательное поле *"),
-    password: Yup.string().required("Обязательное поле *").min(8, "Too short!"),
+  username: Yup.string()
+    .min(2, "Too short!")
+    .max(50, "Too long!")
+    .required("Обязательное поле *"),
+  email: Yup.string()
+    .min(2, "Too short!")
+    .max(50, "Too long!")
+    .required("Обязательное поле *"),
+  password: Yup.string().required("Обязательное поле *").min(8, "Too short!"),
 });
 
 class RegistrationForm extends Component {
-    
-    handleClick = () => {
-        this.props.history.push('/login')
-    };
+  handleClick = () => {
+    this.props.history.push("/login");
+  };
 
-    handleSubmit = (values) => {
-        this.props.register(values)
-        this.props.history.push('/login')
-    };
+  handleSubmit = (values) => {
+    this.props.register(values);
+    this.props.history.push("/login");
+  };
 
-    render () {
-
-        if (this.props.error) {
-            setTimeout(() => {
-              this.props.clearError()
-            }, 3000)
-        };
-
-        return (
-            <>
-                <Notification error={Boolean(this.props.error)} message="Пользователь с такими данными уже существует"></Notification>
-                <Formik 
-                initialValues={{email: "", password: "", username: ""}} 
-                onSubmit={this.handleSubmit} 
-                validationSchema={RegisterSchema}>
-                     {({ errors, touched }) => (
-                <Form className={css.registrationForm}>
-                    <h1>РЕГИСТРАЦИЯ</h1>
-                    <div className={css.registrationInputs}>
-                        <label className={css.formLabel}>
-                            <Field className={`${css.inputField} ${errors.username && touched.username ? css.errorInput : ""}`} 
-                            type="text" 
-                            name="username" 
-                            placeholder="Имя *"/>
-                            <ErrorMessage 
-                            className={css.validField} 
-                            name="username" 
-                            component="span" />
-                        </label>
-                        <label className={css.formLabel}>
-                            <Field className={`${css.inputField} ${errors.email && touched.email ? css.errorInput : ""}`} 
-                            type="email" 
-                            name="email" 
-                            placeholder="E-mail *"/>
-                            <ErrorMessage 
-                            className={css.validField} 
-                            name="email" 
-                            component="span" />
-                        </label>
-                        <label className={css.formLabel}>
-                            <Field 
-                            className={`${css.inputField} ${errors.password && touched.password ? css.errorInput : ""}`} 
-                            type="password" 
-                            name="password" 
-                            placeholder="Пароль *"/>
-                            <ErrorMessage 
-                            className={css.validField} 
-                            name="password" 
-                            component="span" />
-                        </label>
-                    </div>
-                    <div className={css.registrationButtons}>
-                        <Button clickHandler={this.handleClick} >Вход</Button>
-                        <Button type="submit" className="secondary-button">Регистрация</Button>
-                    </div>
-                </Form>
-                )}
-                </Formik>
-            </>
-        )
+  render() {
+    if (this.props.error) {
+      setTimeout(() => {
+        this.props.clearError();
+      }, 3000);
     }
 
-};
+    return (
+      <>
+        <Notification
+          error={Boolean(this.props.error)}
+          message="Пользователь с такими данными уже существует"
+        ></Notification>
+        <Decoration isLoginPage={true}/>
+        <section className="container">
+          <Formik
+            initialValues={{ email: "", password: "", username: "" }}
+            onSubmit={this.handleSubmit}
+            validationSchema={RegisterSchema}
+          >
+            {({ errors, touched }) => (
+              <Form className={css.registrationForm}>
+                <h1>РЕГИСТРАЦИЯ</h1>
+                <div className={css.registrationInputs}>
+                  <label className={css.formLabel}>
+                    <Field
+                      className={`${css.inputField} ${
+                        errors.username && touched.username
+                          ? css.errorInput
+                          : ""
+                      }`}
+                      type="text"
+                      name="username"
+                      placeholder="Имя *"
+                    />
+                    <ErrorMessage
+                      className={css.validField}
+                      name="username"
+                      component="span"
+                    />
+                  </label>
+                  <label className={css.formLabel}>
+                    <Field
+                      className={`${css.inputField} ${
+                        errors.email && touched.email ? css.errorInput : ""
+                      }`}
+                      type="email"
+                      name="email"
+                      placeholder="E-mail *"
+                    />
+                    <ErrorMessage
+                      className={css.validField}
+                      name="email"
+                      component="span"
+                    />
+                  </label>
+                  <label className={css.formLabel}>
+                    <Field
+                      className={`${css.inputField} ${
+                        errors.password && touched.password
+                          ? css.errorInput
+                          : ""
+                      }`}
+                      type="password"
+                      name="password"
+                      placeholder="Пароль *"
+                    />
+                    <ErrorMessage
+                      className={css.validField}
+                      name="password"
+                      component="span"
+                    />
+                  </label>
+                </div>
+                <div className={css.registrationButtons}>
+                  <Button clickHandler={this.handleClick}>Вход</Button>
+                  <Button type="submit" className="secondary-button">
+                    Регистрация
+                  </Button>
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </section>
+      </>
+    );
+  }
+}
 
 const mapDispatchToProps = {
-    clearError: authActions.clearError,
-    register: ops.register,
+  clearError: authActions.clearError,
+  register: ops.register,
 };
 
-const mapStateToProps = state => ({
-    error: globalSelectors.getError(state),
+const mapStateToProps = (state) => ({
+  error: globalSelectors.getError(state),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(RegistrationForm);
