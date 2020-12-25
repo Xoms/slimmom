5fd062937097193d9b86189f78625d5d6f1f4bac
Xoms <cilcksensead@gmail.com>
2020-12-25 16:47:23 +0200

    tmp

--- src/pages/CalculatorPage/CalculatorPage.jsx
+++ src/pages/CalculatorPage/CalculatorPage.jsx
@@ -9,29 +9,35 @@ import userSelectors from '../../redux/user/userSelectors';
 import { getProducts } from '../../redux/user/userOperations';
 
 class CalculatorPage extends Component {
+  
+
   componentDidMount() {
     if (!this.props.dayId) {
       const today = new Date().toJSON().slice(0, 10);
       this.props.getProducts({ date: today });
     }
+    // } else {
+    // this.dailyNorm = this.getDailyNorm();
+    // }
+    // console.log(this.dailyNorm);
   }
 
-  getDailyNorm() {
-    if (this.props.summary) {
-      return this.props.summary;
-    }
-    return this.props.daySummary;
-  }
+  // getDailyNorm() {
+  //   if (this.props.summary) {
+  //     return this.props.summary;
+  //   }
+  //   return this.props.daySummary;
+  // }
 
   render() {
-    const dailyNorm = { ...this.getDailyNorm() };
+    // const dailyNorm = { ...this.getDailyNorm() };
     return (
       <Fragment>
         <Decoration isCalculationPage={true} />
         <section className="container">
           <div className={styles.wrapper}>
             <DailyCaloriesForm noModal={true} />
-            <RightSideBar {...dailyNorm} />
+            <RightSideBar />
           </div>
         </section>
       </Fragment>
@@ -48,10 +54,10 @@ class CalculatorPage extends Component {
 }
 
 const mapStateToProps = state => ({
-  summmary: userSelectors.getCurrentDaySummary(state),
-  //summmary: userSelectors.getSummary(state),
+  // summmary: userSelectors.getCurrentDaySummary(state),
+  // //summmary: userSelectors.getSummary(state),
   dayId: userSelectors.getCurrentDayId(state),
-  daySummary: userSelectors.getDaySummary(state),
+  // daySummary: userSelectors.getDaySummary(state),
 });
 
 const mapDispatchToProps = {
