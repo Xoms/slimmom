3a4577baccf4ecf59eeff8a93ff83ed055891823
VolodymyrKoch <volodymyrkocherhan@gmail.com>
2020-12-25 13:30:21 +0200

    fixed css sideBar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,51 +1,58 @@
-import React, { Component, Fragment } from "react"
-import Media from "react-media"
+import React, { Component, Fragment } from 'react';
+import Media from 'react-media';
 // import PropTypes from "prop-types"
-import { NavLink } from "react-router-dom"
-import { BurgerMenu, Burger } from "../BurgerMenu"
-import { Logo, LogoText } from "../Logo"
+import { NavLink } from 'react-router-dom';
+import { BurgerMenu, Burger } from '../BurgerMenu';
+import { Logo, LogoText } from '../Logo';
 import NavigationBar from '../NavigationBar';
 import UserInfo from '../UserInfo';
 import AuthNavigations from '../AuthNavigations';
-import styles from "./Header.module.scss"
-import withAuth from "../hocs/withAuth"
+import styles from './Header.module.scss';
+import withAuth from '../hocs/withAuth';
 
 class Header extends Component {
   state = {
     isOpen: false,
-  }
+  };
   handleToggle = () => {
-    this.setState({ isOpen: !this.state.isOpen })
-  }
+    this.setState({ isOpen: !this.state.isOpen });
+  };
 
   render() {
-    const { isAuth } = this.props
-    const { isOpen } = this.state
+    const { isAuth } = this.props;
+    const { isOpen } = this.state;
 
     return (
       <>
-        <header className='container'>
+        <header className="container">
           <div className={styles.mainHeader}>
             <NavLink exact to="/" className={styles.logoLink}>
               <div className={styles.logoContainer}>
-
                 <Media
                   queries={{
-                    small: "(min-width: 320px) and (max-width: 767px)",
-                    medium: "(min-width: 768px) and (max-width: 1099px)",
-                    large: "(min-width: 1100px)",
+                    small: '(min-width: 320px) and (max-width: 649px)',
+                    medium: '(min-width: 650px) and (max-width: 1099px)',
+                    large: '(min-width: 1100px)',
                   }}
                 >
-                  {(matches) => (
+                  {matches => (
                     <Fragment>
                       {matches.small && (
-                        <><Logo /> {isAuth && <LogoText />}</>
+                        <>
+                          <Logo /> {isAuth && <LogoText />}
+                        </>
                       )}
                       {matches.medium && (
-                        <><Logo /><LogoText /></>
+                        <>
+                          <Logo />
+                          <LogoText />
+                        </>
                       )}
                       {matches.large && (
-                        <><Logo /><LogoText /></>
+                        <>
+                          <Logo />
+                          <LogoText />
+                        </>
                       )}
                     </Fragment>
                   )}
@@ -53,89 +60,124 @@ class Header extends Component {
               </div>
             </NavLink>
             <div>
-            <Media
-                  queries={{
-                    small: "(min-width: 320px) and (max-width: 767px)",
-                    medium: "(min-width: 768px) and (max-width: 1099px)",
-                    large: "(min-width: 1100px)",
-                  }}
-                >
-                  {(matches) => (
-                    <Fragment>
-                      {matches.small && (
-                        <>{isAuth ? (
+              <Media
+                queries={{
+                  small: '(min-width: 320px) and (max-width: 649px)',
+                  medium: '(min-width: 650px) and (max-width: 1099px)',
+                  large: '(min-width: 1100px)',
+                }}
+              >
+                {matches => (
+                  <Fragment>
+                    {matches.small && (
+                      <>
+                        {isAuth ? (
                           <div className={styles.navigationAuth}>
                             {!isOpen ? (
-                              <Burger onClick={this.handleToggle}/>
+                              <Burger onClick={this.handleToggle} />
                             ) : (
-                              <button type="button" className={styles.closeBtn} onClick={() => this.handleToggle()}>
+                              <button
+                                type="button"
+                                className={styles.closeBtn}
+                                onClick={() => this.handleToggle()}
+                              >
                                 X
                               </button>
                             )}
                           </div>
                         ) : (
-                          <div className={styles.navigation}> <NavigationBar /></div> //тут будет комп NAV
+                          <div className={styles.navigation}>
+                            {' '}
+                            <NavigationBar />
+                          </div> //тут будет комп NAV
                         )}
                         {/* {isAuth && <div className={styles.mobUserInfoWrap}><UserInfo /></div>}  */}
-                        </>
-                      )}
-                      {matches.medium && (
-                        <>{isAuth ? (
+                      </>
+                    )}
+                    {matches.medium && (
+                      <>
+                        {isAuth ? (
                           <div className={styles.navigationAuth}>
                             {/* сюда вставить UserMenu */}
-                            {isAuth && <div><UserInfo /></div>}
+                            {isAuth && (
+                              <div>
+                                <UserInfo />
+                              </div>
+                            )}
 
                             {!isOpen ? (
-                              <Burger onClick={this.handleToggle}/>
+                              <Burger onClick={this.handleToggle} />
                             ) : (
-                              <button type="button" className={styles.closeBtn} onClick={() => this.handleToggle()}>
+                              <button
+                                type="button"
+                                className={styles.closeBtn}
+                                onClick={() => this.handleToggle()}
+                              >
                                 X
                               </button>
                             )}
                           </div>
                         ) : (
-                          <div className={styles.navigation}> <NavigationBar /> </div> //тут будет комп NAV
-                        )}</>
-                      )}
-                      {matches.large && (
-                        <>{isAuth ? (
+                          <div className={styles.navigation}>
+                            {' '}
+                            <NavigationBar />{' '}
+                          </div> //тут будет комп NAV
+                        )}
+                      </>
+                    )}
+                    {matches.large && (
+                      <>
+                        {isAuth ? (
                           <div className={styles.navigationAuth}>
-                            {isAuth && <>
-                            <div className={styles.navigation}> <AuthNavigations /> </div>
-                            <div><UserInfo /></div>
-                            </>}
+                            {isAuth && (
+                              <>
+                                <div className={styles.navigation}>
+                                  {' '}
+                                  <AuthNavigations />{' '}
+                                </div>
+                                <div>
+                                  <UserInfo />
+                                </div>
+                              </>
+                            )}
                           </div>
                         ) : (
-                          <div className={styles.navigation}> <NavigationBar /> </div>
-                        )}</>
-                      )}
-                    </Fragment>
-                  )}
-                </Media>
-
+                          <div className={styles.navigation}>
+                            {' '}
+                            <NavigationBar />{' '}
+                          </div>
+                        )}
+                      </>
+                    )}
+                  </Fragment>
+                )}
+              </Media>
             </div>
           </div>
-
         </header>
-        {<Media
-                  queries={{
-                    small: "(min-width: 320px) and (max-width: 767px)",
-                    medium: "(min-width: 768px) and (max-width: 1099px)",
-                    large: "(min-width: 1100px)",
-                  }}
-                >
-                {(matches) => (
-                  <Fragment>
-                  {matches.small && isAuth && <div className={styles.mobUserInfoWrap}><UserInfo /></div>}
-                  </Fragment>
-                  )
-                }
-
-        </Media>}
-        {this.state.isOpen && <BurgerMenu onClick={this.handleToggle}/>}
+        {
+          <Media
+            queries={{
+              small: '(min-width: 320px) and (max-width: 649px)',
+              medium: '(min-width: 650px) and (max-width: 1099px)',
+              large: '(min-width: 1100px)',
+            }}
+          >
+            {matches => (
+              <Fragment>
+                {matches.small && isAuth && (
+                  <div className={styles.mobUserInfoWrap}>
+                    <UserInfo />
+                  </div>
+                )}
+              </Fragment>
+            )}
+          </Media>
+        }
+        {this.state.isOpen && <BurgerMenu onClick={this.handleToggle} />}
       </>
-    )
+    );
   }
 }
 
-export default withAuth(Header)
+export default withAuth(Header);
